fafb2263358e3df40e776650888098131eb9a82c
Josuah Demangeon <me@josuah.net>
2025-04-27 01:44:15 +0200

    soc: tinyvision: tinyclunx33: rtlv1.1 booting at 0x200000
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- drivers/video/tvai_uvcmanager.h
+++ drivers/video/tvai_uvcmanager.h
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef ZEPHYR_DRIVERS_VIDEO_UVCMANAGER_H
-#define ZEPHYR_DRIVERS_VIDEO_UVCMANAGER_H
+#ifndef ZEPHYR_DRIVERS_VIDEO_TVAI_UVCMANAGER_H
+#define ZEPHYR_DRIVERS_VIDEO_TVAI_UVCMANAGER_H
 
 void uvcmanager_lib_init(uint32_t base, uint32_t fifo);
 void uvcmanager_lib_start(uint32_t base, uint32_t trb_addr, uint32_t depupdxfer, uint32_t depcmd);
@@ -15,4 +15,4 @@ void uvcmanager_lib_set_format(uint32_t base, uint32_t pitch, uint32_t height);
 void uvcmanager_lib_read(uint32_t base, uint8_t *buf_data, size_t buf_size);
 void uvcmanager_lib_cmd_show(uint32_t base, const struct shell *sh);
 
-#endif /* ZEPHYR_DRIVERS_VIDEO_UVCMANAGER_H */
+#endif /* ZEPHYR_DRIVERS_VIDEO_TVAI_UVCMANAGER_H */
