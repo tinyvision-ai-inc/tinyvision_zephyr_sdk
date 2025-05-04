55a992009eef43f7c128516356bd0a763b9736f5
Vdragon <mail@massdriver.space>
2025-05-04 21:08:47 +0200

    Add west debug support for tinyclunx33

--- boards/tinyvision/tinyclunx33/board.cmake
+++ boards/tinyvision/tinyclunx33/board.cmake
@@ -3,3 +3,8 @@
 
 include(${ZEPHYR_BASE}/boards/common/ecpprog.board.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../../common/ecpprog_mpremote.board.cmake)
+
+
+board_runner_args(openocd --cmd-pre-init "source [find tinyclunx33.cfg]")
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
